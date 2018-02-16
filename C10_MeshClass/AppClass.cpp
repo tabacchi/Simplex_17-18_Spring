761eacd700e5c9562b130895382b1ad21e988f69
thomas tabacchi <tabacchi0128@gmail.com>
2018-02-16 14:50:19 -0400

    E04 done

--- C10_MeshClass/AppClass.cpp
+++ C10_MeshClass/AppClass.cpp
@@ -7,7 +7,7 @@ void Application::InitVariables(void)
 
 	//Make MyMesh object
 	m_pMesh1 = new MyMesh();
-	m_pMesh1->GenerateCube(1.0f, C_WHITE);
+	m_pMesh1->GenerateCube(1.0f, C_BLACK);
 }
 void Application::Update(void)
 {
@@ -25,9 +25,72 @@ void Application::Display(void)
 	// Clear the screen
 	ClearScreen();
 
-	m_pMesh->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), ToMatrix4(m_qArcBall));
-	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3( 3.0f, 0.0f, 0.0f)));
-		
+	static float value = 0.0f;
+
+	//bottom teeth things
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4,vector3(3.0f+value, 0.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(4.0f + value, 0.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(1.0f + value, 0.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(0.0f + value, 0.0f, 0.0f)));
+	//above teeth
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-3.0f + value, 1.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-1.0f + value, 1.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(7.0f + value, 1.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(5.0f + value, 1.0f, 0.0f)));
+	//mid-1
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(2.0f + value, 2.0f, 0.0f)));
+	//mid-1 left
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(1.0f + value, 2.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(0.0f + value, 2.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-1.0f + value, 2.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-3.0f + value, 2.0f, 0.0f)));
+	//mid-1 right
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(3.0f + value, 2.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(4.0f + value, 2.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(5.0f + value, 2.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(7.0f + value, 2.0f, 0.0f)));
+	//mid mid
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(2.0f + value, 3.0f, 0.0f)));
+	//mid left
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(1.0f + value, 3.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(0.0f + value, 3.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-1.0f + value, 3.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-2.0f + value, 3.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-3.0f + value, 3.0f, 0.0f)));
+	//mid right
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(3.0f + value, 3.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(4.0f + value, 3.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(5.0f + value, 3.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(6.0f + value, 3.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(7.0f + value, 3.0f, 0.0f)));
+	//+1mid
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(2.0f + value, 4.0f, 0.0f)));
+	//+1mid left
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(1.0f + value, 4.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-1.0f + value, 4.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-2.0f + value, 4.0f, 0.0f)));
+	//+1mid right
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(3.0f + value, 4.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(5.0f + value, 4.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(6.0f + value, 4.0f, 0.0f)));
+	//+2mid
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(2.0f + value, 5.0f, 0.0f)));
+	//+2mid left
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(1.0f + value, 5.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(0.0f + value, 5.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-1.0f + value, 5.0f, 0.0f)));
+	//+2mid right
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(3.0f + value, 5.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(4.0f + value, 5.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(5.0f + value, 5.0f, 0.0f)));
+	//ears
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(0.0f + value, 6.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(-1.0f + value, 7.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(4.0f + value, 6.0f, 0.0f)));
+	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(IDENTITY_M4, vector3(5.0f + value, 7.0f, 0.0f)));
+	
+	value += 0.01f;
+
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
 	
